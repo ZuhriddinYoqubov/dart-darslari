1d3a1f9b9acee45645c13f146430ca05da83cf3f
Zuhriddin91 <turoni1991@gmail.com>
2021-08-09 17:23:08 +0500

    15-dars

--- lesson_15/lesson_15-2.dart
+++ lesson_15/lesson_15-2.dart
@@ -1,5 +1,5 @@
 main(List<String> args) async {
-  
+  /*
   var funk1 = (int s) async => s + 10;
   var funk2 = (int q) async => q + 40;
   var funk3 = (int z) async => Future.delayed(
@@ -12,6 +12,23 @@ main(List<String> args) async {
   value = await funk3(1000); //assignment
   print(value);
   futureKirit();
+  */
+
+videodownload();
+
+}
+void videodownload() async{
+  var video = new Future.value('100 mb').then((v1) {
+    Future.delayed(Duration(seconds: 1), () => print(v1)); 
+    return new Future.value('$v1 200 mb').then((v2) {
+      Future.delayed(Duration(seconds: 2), () => print(v2));
+      return new Future.value('$v2 300 mb').then((v3) {
+        Future.delayed(Duration(seconds: 3), () => print(v3));
+        return new Future.value('$v3 400 mb');
+      });
+    });
+  });
+  video.then((value) => print("Video yuklandi: $value"));
 }
 
 void futureKirit(){
